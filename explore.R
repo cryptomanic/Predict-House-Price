63df174d17ecd3cf47e27f55db818b6827afb55b
cryptomanic <deepakyadav.iitbhu@gmail.com>
2017-01-04 12:27:12 +0530

    use of two explanatory variable for prediction

--- explore.R
+++ explore.R
@@ -25,8 +25,8 @@ fmodel(model2, ~BedroomAbvGr, data = train) +
   geom_point(aes(y = SalePrice, x = BedroomAbvGr), data = train)
 
 cerr <- cv_pred_error(model1, model2)
-t.test(mse~model, cerr) # since p-value is high
-# we fail to reject the null hypothesis which is 'prediction error is same'
+t.test(mse~model, cerr) # since p-value is low
+# we will reject the null hypothesis which is 'prediction error is same'
 
 # predict output for training data
 output1 <- evaluate_model(model1, data = train)
@@ -41,3 +41,24 @@ output <- evaluate_model(model1, data = test)
 
 df <- data.frame(Id = test$Id, SalePrice = output$model_output)
 write.csv(x = df, file = "model1.csv", quote = F, row.names = F)
+
+# Wed Jan  4 11:48:01 2017 ------------------------------
+# CentralAir: Central air conditioning
+
+ggplot(train, aes(x = factor(1), fill = factor(CentralAir))) + 
+  geom_bar(width = 1) + coord_polar(theta = "y") + theme_void()
+
+train$CentralAir <- as.factor(train$CentralAir)
+
+model3 <- lm(SalePrice ~ CentralAir + BedroomAbvGr, data = train)
+fmodel(model3, ~BedroomAbvGr + CentralAir)
+
+# predict output for training data
+output3 <- evaluate_model(model3, data = train)
+
+# predict output for test data
+test$CentralAir <- as.factor(test$CentralAir)
+output <- evaluate_model(model3, data = test)
+
+df <- data.frame(Id = test$Id, SalePrice = output$model_output)
+write.csv(x = df, file = "model3.csv", quote = F, row.names = F)
